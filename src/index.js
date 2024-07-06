2806c04a99b751eb7c5ccf48c250985375e470bf
Awointa <akosawointa@gmail.com>
2024-07-06 02:20:06 +0100

    built the pizza component

--- src/index.js
+++ src/index.js
@@ -56,7 +56,13 @@ function App() {
 }
 
 function Pizza() {
-	return <h2>Pizza</h2>;
+	return (
+		<div>
+		<img src="pizzas/spinaci.jpg" alt="pizza spinachi" />
+		<h2>Pizza Spinaci</h2>
+		<p>Tomato, mozarella, spinach, and ricotta cheese</p>
+		</div>
+	);
 }
 
 const root = ReactDom.createRoot(document.getElementById("root"));
